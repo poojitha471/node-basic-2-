e7365e8d3058421a248e7c8de7f765fdc0e5d5c6
poojitha <poojitha471@gmail.com>
2022-06-12 21:24:48 +0530

    first

---
+++ routes/recipes.js
@@ -0,0 +1,37 @@
+var recipes = require('../recipes.json');
+var router = require('express').Router();
+
+
+router.get('/shopping-list', function (req, res, next) {
+    try {
+  
+      let resultArray = [];
+  
+      if (!req.query.ids) {
+        res.status(400).send({ "Status Code": 400, Body: "ids are required" });
+      }
+      else {
+        let ids = req.query.ids;
+        let inputArray = ids.split(',');
+        for (let i = 0; i < inputArray.length; i++) {
+          let obj = recipes.filter(x => x.id === Number(inputArray[i]))[0];
+          if (obj != undefined) {
+            resultArray.push(obj)
+          }
+        }
+  
+        if (resultArray.length === 0) {
+          res.status(404).send({ "Status Code": 404, Body: "NOT_FOUND" });
+        }
+        else {
+          res.status(200).send({ "Status Code": 200, Body: resultArray });
+        }
+      }
+    }
+    catch (e) {
+      res.status(400).send({ "Status Code": 400, Body: e.message });
+    }
+  });
+  
+module.exports = router;
+
